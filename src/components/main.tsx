95ef5bad83ea04b788f26b55b547c29c776ecc60
Vincent Lee <zarokigaro@gmail.com>
2022-05-21 23:13:40 +0800

    Update main.tsx

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -8,24 +8,32 @@ import { getOpenWeatherService } from "./api";
 import { FormField } from "../_types";
 import axios from "axios";
 import { initialValues } from "../_constants";
+import _ from "lodash";
 
 export default function WeatherPage(): ReactElement {
   const [weather, setWeather] = useState(null);
   const [historyList, setHistoryList] = useState<any>([]);
   const [formValue, setFormValue] = useState<FormField>(initialValues);
+  const [isError, setIsError] = useState<string>('');
+
   async function fetchWeather(values: FormField) {
     axios
       .get(getOpenWeatherService(values.city, values.country))
       .then((response: { data: any }) => {
+        saveHistoryList({
+          city: response.data.name,
+          country: response.data.sys.country,
+        });
         setWeather(response.data);
       })
-      .catch((error: any) => console.error(error));
+      .catch((error: any) => {
+        setIsError(error.response.data.message);
+        console.error(error.response.data.message);
+      });
   }
 
-  async function handleSubmit(values: FormField) {
-    setFormValue(values);
-    fetchWeather(values);
-
+  function saveHistoryList(values: { city: string; country: string }) {
+    if (_.filter(historyList, _.matches(values))) return;
     const currentTime = moment().format("YYYY-MM-DD h:mm:ss a");
     setHistoryList((prevState: any) => [
       ...prevState,
@@ -37,19 +45,26 @@ export default function WeatherPage(): ReactElement {
     ]);
   }
 
+  async function handleSubmit(values: FormField) {
+    setIsError("");
+    setFormValue(values);
+    fetchWeather(values);
+  }
+
   function handleSearch(values: FormField) {
-    handleSubmit(values)
+    handleSubmit(values);
   }
 
   function handleClear() {
     setFormValue(initialValues);
-    setWeather(null)
+    setWeather(null);
+    setIsError("");
   }
 
   function handleDelete(index: any) {
-    const temp = [...historyList];
-    temp.splice(index, 1);
-    setHistoryList(temp);
+    const tempList = [...historyList];
+    tempList.splice(index, 1);
+    setHistoryList(tempList);
   }
 
   return (
@@ -63,7 +78,7 @@ export default function WeatherPage(): ReactElement {
         setWeather={setWeather}
         setHistoryList={setHistoryList}
       />
-      <Preview queryResult={weather} />
+      {isError.length ? <p> {isError || 'Not Found'} </p> : <Preview queryResult={weather} />}
       <History
         historyList={historyList}
         handleDelete={handleDelete}
